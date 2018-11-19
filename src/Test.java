669fb2779dbaae387288fffead904eba61590faa
Kevin Li <kli8170@bths.edu>
2018-11-19 14:27:16 -0500

    Sorting Project

---
+++ src/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        int[] hello = {1,4,7,2,6,4,2,7,8,9,12,1,4,3};
+
+        Sorts.ascendingSort(hello);
+        Sorts.printArray(hello);
+    }
+}
