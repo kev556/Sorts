983d639cbeaf56251d4cd366c804213d78856d6a
Kevin Li <kli8170@bths.edu>
2018-11-28 14:41:26 -0500

    Adding Utilities

---
+++ src/Utilities.java
@@ -0,0 +1,25 @@
+package algebra;
+
+public class Utilities
+{
+    public static String getNumericSuffix(int num)
+    {
+        if (num % 100 != 11 && num % 10 != 1)
+        {
+            return "st";
+        }
+        else if (num % 100 != 12 && num % 10 != 2)
+        {
+            return "nd";
+        }
+        else if (num % 100 != 13 && num % 10 != 3)
+        {
+            return "rd";
+        }
+        return "st";
+    }
+    public static int getRandomNumber(int maxDigits)
+    {
+        return (int)(Math.random() * Math.pow(10, maxDigits));
+    }
+}
