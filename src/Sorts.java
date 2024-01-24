b02ec8e43410c2ace0291a49cffe82e29b9e996e
kev556 <26943287+kev556@users.noreply.github.com>
2024-01-24 18:46:02 -0500

    added mergesort

--- src/Sorts.java
+++ src/Sorts.java
@@ -50,14 +50,15 @@ public class Sorts
     }
 	
     public static void bubbleSort(int[] elements) {
-	for (int i = 0; i < elements.length - 1; i++) {
-		for (int j = 0; j < elements.length - i - 1; j++) {
-			if (elements[j] > elements[j + 1]) {
-				int temp = elements[j];
-				elements[j] = elements[j + 1];
-				elements[j + 1] = temp;
-			}
-		}
+        for (int i = 0; i < elements.length - 1; i++) {
+            for (int j = 0; j < elements.length - i - 1; j++) {
+                if (elements[j] > elements[j + 1]) {
+                    int temp = elements[j];
+                    elements[j] = elements[j + 1];
+                    elements[j + 1] = temp;
+                }
+            }
+        }
 	}	
 
     public static void swap(int indexA,int indexB, int[] arr) {
