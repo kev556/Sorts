0bfe28c92b6a9702217d39b13a640e04106a449c
kev556 <26943287+kev556@users.noreply.github.com>
2024-01-24 19:09:18 -0500

    added bucket sort

--- src/Sorts.java
+++ src/Sorts.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 
+import javax.swing.text.Utilities;
+
 public class Sorts
 {
     public static void selectionSort(int[] arr) {
@@ -59,8 +61,82 @@ public class Sorts
                 }
             }
         }
-	}	
+	}
+    
+    public static void mergeSort(int[] elements, int left, int right) {
+        if (left < right) {
+            int middle = (l + r) / 2;
 
+            mergeSort(elements, left, right);   // sort left half
+            mergeSort(elements, middle + 1, right); // sort right half
+            merge(elements, left, middle, right);    // merge sorted halfs
+        }
+        return arr;
+    }
+    public static void merge(int[] arr, int l, int m, int r) { 
+        
+        // Find lengths of two subarrays to be merged
+        int length1 = m - l + 1;
+        int length2 = r - m;
+
+        // Create temp arrays 
+        int L[] = new int[length1];
+        int R[] = new int[length2];
+
+        // Copy the sorted left & right halfs to temp arrays
+        for (int i = 0; i < length1; i++) {
+            L[i] = arr[l + i];
+        }
+        
+        for (int j = 0; j < length2; j++) {
+            R[j] = arr[m + 1 + j];
+        }
+        
+        // initial indexes of left and right sub-arrays
+        int i = 0; // index for left
+        int j = 0; // index for right
+        int k = l; // Initial index of merged subarray array
+
+        // Merge the two sorted halfs into the original array
+        while(i < length1 && j < length2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+        // One of the halfs will have elements remaining
+
+        // Copy remaining elements of L[] if any 
+        while (i < length1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        // Copy remaining elements of R[] if any 
+        while (j < length2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+    public static void bucketSort(int[] elements) {
+        int[] temp = new int[3];
+        for (int n : elements)
+            temp[n]++;
+
+        int i = 0;
+        for (int n = 0; n < temp.length; n++) {
+            for (int j = 0; j < temp[n]; j++) {
+                elements[i] = n;
+                i++;
+            }
+        }
+    }
     public static void swap(int indexA,int indexB, int[] arr) {
         int temp = arr[indexA];
         arr[indexA] = arr[indexB];
